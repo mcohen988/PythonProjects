fb781976371b6c6e9bdbfeb20320b2fb253cfa1c
mcohen988 <93428389+mcohen988@users.noreply.github.com>
2021-10-31 17:10:06 +0200

    Add files via upload
    A simple program to validate that a user-specified email address is valid.

---
+++ emailvalidation.py
@@ -0,0 +1,64 @@
+def has_invalid_characters(string):
+    valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-."
+    
+    # Detecting invalid characters
+    for char in string:
+        if char not in valid:
+            return True
+    return False
+
+def is_valid(email):
+    if email.count("@") != 1:
+        return False
+    # Tuple unpacking - prefix, domain
+    prefix, domain = email.split("@")
+    
+    # Checking the domain and prefix for invalid characters 
+    if has_invalid_characters(prefix):
+        return False
+    
+    if has_invalid_characters(domain):
+        return False
+
+    # Checking the length of prefix and domain for invalid syntax
+
+    if len(prefix) == 0:
+        return False
+
+    if domain.count(".") != 1:
+        return False
+
+    if domain.endswith("."):
+        return True
+            
+    domain_name, extension = domain.split(".")
+    if len(domain_name) == 0 or len(extension) == 0:
+        return False
+    return True
+    
+
+emails = [
+    "test@example.com",
+    "valid@gmail.com",
+    "invalid@gmail",
+    "invalid",
+    "not an email",
+    "invalid@email",
+    "!@/",
+    "test@@example.com",
+    "test@.com",
+    "test@site.",
+    "@example.com",
+    "an.example@test",
+    "te#st@example.com",
+    "test@exam!ple.com",
+    "moshe@walla.co.il",
+    "MOSH@GMAIL.COM"
+]
+
+for email in emails:
+    if is_valid(email):
+        print(email + " is valid")
+    else:
+        print(email + " is invalid")
+        
