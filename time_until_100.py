9dcf37864b50d89b510f0f2dd6efcbd6a2dbbead
mcohen988 <93428389+mcohen988@users.noreply.github.com>
2021-10-31 17:14:57 +0200

    Add files via upload

---
+++ time_until_100.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+currentYear = datetime.now().year
+
+name = input("What's your name? ")
+age = int(input("How old are you? "))
+time_until_100 = 100 - int(age)
+
+print(name + " You will turn 100 in the year " + str(currentYear + time_until_100))
