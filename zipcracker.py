2cd3b8ebb8d1c82d1c32fd2033bd26fb5f29ef84
mcohen988 <93428389+mcohen988@users.noreply.github.com>
2021-12-10 14:55:07 +0200

    Add files via upload

---
+++ zipcracker.py
@@ -0,0 +1,26 @@
+import zipfile
+
+# Attempting to extract the contents
+def extractZip(zfile, password):
+    try:
+        zfile.extractall(pwd=bytes(password, 'utf-8'))
+        return password
+    except:
+        print("[-] Wrong Password")
+        return
+
+# Looping over the extractZip() function with a wordlist file
+def main():
+    zfile = zipfile.ZipFile('passwords.zip')    # Change filename
+    passFile = open('passwords.txt')            # Change wordlist
+    for line in passFile.readlines():
+        password = line.strip('\n')
+        guess = extractZip(zfile, password)
+        if guess:
+            print("[+] The password is: " + password)
+            zfile.printdir()
+            break
+
+
+if __name__ == '__main__':
+    main()
